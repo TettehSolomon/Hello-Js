525347675c7627c687c6038ccb0ed8194ba59361
TettehSolomon <tettehsolomon106@gmail.com>
2024-02-27 09:10:30 +0100

    initial commit

---
+++ script/main.js
@@ -0,0 +1,71 @@
+console.log('<<<<<<<<<<<<<<<<< >>>>>>>>>>>>>>>> \n Welcome to GMC Micro Finance Bank \n <<<<<<<<<<<<<<<<< >>>>>>>>>>>>>>>>');
+
+/*+++++++++++++++++
+    ATM Menu
++++++++++++++++++*/
+console.log('The Automated Teller Machine (ATM) Menu');
+console.log('(1) Check Balance');
+console.log('(2) Deposit');
+console.log('(3) Withdraw');
+console.log('(4) Transfer');
+console.log('(5) Quit');
+console.log('(6) Create Account');
+
+/*++++++++++++++++++++++++
+    Create A New Account
+++++++++++++++++++++++++++*/
+console.log('Would you like to create a new account? \n y or n');
+var createAccount = prompt('Would you like to create a new account? \n y or n');
+console.log(createAccount);
+
+/*+++++++++++++++++++++++++
+    Provide your Full Name
++++++++++++++++++++++++++++*/
+console.log('Provide your full name');
+var fullName = prompt('Provide your fullname');
+console.log(fullName);
+
+/*++++++++++++++++++++++++++++++
+    Provide your Email Address
++++++++++++++++++++++++++++++++*/
+console.log('Provide your email address');
+var email = prompt('Provide your email address');
+console.log(email);
+
+/*+++++++++++++++++++++++++++
+    Provide your Phone Number
++++++++++++++++++++++++++++++*/
+console.log('Provide your Phone Number');
+var phoneNumber = prompt('Provide your Phone Number');
+console.log(phoneNumber);
+
+/*+++++++++++++++++++++++++++
+    Provide your BVN
++++++++++++++++++++++++++++++*/
+console.log('Provide your BVN');
+var bvn = prompt('Provide your BVN');
+console.log(bvn);
+
+/*+++++++++++++++++++++++++++
+    Create a New PIN
++++++++++++++++++++++++++++++*/
+console.log('Create a New PIN');
+var pin = prompt('Create a new PIN');
+console.log(pin);
+
+/*+++++++++++++++++++++++++++
+    Confirm New PIN
++++++++++++++++++++++++++++++*/
+console.log('Confirm New PIN');
+var confirmPin = prompt('Confirm New PIN');
+console.log(confirmPin);
+
+/*+++++++++++++++++++++++++++
+    Success Message
++++++++++++++++++++++++++++++*/
+console.log('Hello ' + fullName + '.' + 'Your account has been succesfully created');
+var fullName = alert('Hello ' + fullName + '.' + ' Your account has been succesfully created');
+console.log(fullName);
+
+
+

6126f4725a4d9432f968567f1a3fedcbe86ecc29
TettehSolomon <tettehsolomon106@gmail.com>
2024-02-28 08:59:50 +0100

    hello js

--- script/main.js
+++ script/main.js
@@ -1,10 +1,19 @@
-let num1 = parseInt(prompt('num1'));
-let num2 = parseInt(prompt('num2'));
-let result = num1 + num2;
+// let num1 = prompt('num1');
+// let num2 = prompt('num2');
+// let firstNumber = parseInt((num1));
+// let secondNumber = parseInt((num2));
+
+// let result = firstNumber + secondNumber;
+// alert(result);
 
 
-window.alert(result);
+let water = "Water is here";
+let juice = "Juice is here";
 
+[water, juice] = [juice, water];
 
+console.log(water);
+alert(water);
 
-
+console.log(juice);
+alert(juice);
